86929e9fb10c490ec6f05d78815d3f47cf062716
Michael <m.adamsmadden@gmail.com>
2024-11-19 12:24:08 -0600

    BAXTER-18 - Added logout and redirects for login and registration

--- app/auth/views.py
+++ app/auth/views.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.views import LoginView
-from django.contrib import messages
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
 from django.views.generic import CreateView
 
@@ -14,3 +14,8 @@ class RegisterView(CreateView):
     form_class = RegisterForm
     template_name = 'auth/register.html'
     success_url = reverse_lazy('auth:login')
+
+    def dispatch(self, request, *args, **kwargs):
+        if request.user.is_authenticated:
+            return redirect('home:home')
+        return super(RegisterView, self).dispatch(request, *args, **kwargs)
--- app/settings.py
+++ app/settings.py
@@ -137,5 +137,6 @@ STATICFILES_DIRS = [
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
-LOGIN_URL="auth/login/"
+LOGIN_URL="/auth/login/"
 LOGIN_REDIRECT_URL = "/"
+LOGOUT_REDIRECT_URL="/auth/login/"
